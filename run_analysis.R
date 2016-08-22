55eae18149f9442aec1c77534c7d9d3701a22184
peyron <kristoffer.peyron@gmail.com>
2016-08-22 13:37:03 +0200

    tidy_data.txt added

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-## Read Data
+## Read data and merge columns
 
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 
@@ -6,23 +6,19 @@ features <- read.table("UCI HAR Dataset/features.txt")
 
 featuresKeep <- grep(".*mean.*|.*std.*", features[,2])
 
-
-## Data
-
-# Train
 trainX <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresKeep]
 trainLabels <- read.table("UCI HAR Dataset/train/y_train.txt")
 trainSubject <- read.table("UCI HAR Dataset/train/subject_train.txt")
 train <- cbind(trainSubject, trainLabels, trainX)
 
-
-# Test
 testX <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresKeep]
 testLabels <- read.table("UCI HAR Dataset/test/y_test.txt")
 testSubject <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(testSubject, testLabels, testX)
 
-#Merge, set column names and label activites
+# Merge rows, keep only columns which include mean and std,
+# set column names and label activites
+
 merged <- rbind(train,test)
 
 featuresKeepNames <- features[featuresKeep,2]
@@ -41,10 +37,12 @@ merged$Activity <- factor(merged$Activity,
                     labels = activityLabels[,2])
 
 ## Create tidy data set with the average of each variable for each subject and activity
+
 require(reshape2)
 mergedMolten<- melt(merged, id = c("Subject_ID", "Activity")) 
 mergedMean <- dcast(mergedMolten, Subject_ID + Activity ~ variable, mean)
 
 ## Write new tidy data to text file
-write.table(mergedMean)
+
+write.table(mergedMean, "Getting-and-Cleaning-Data-Course-Assignment/tidy_data.txt", row.names = FALSE)
 
